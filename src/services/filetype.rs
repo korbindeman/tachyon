686373e1dd6c595901823f06b0c47d03357aee40
Korbin de Man <korbindeman4@gmail.com>
2025-04-24 00:21:32 +0200

    add MIME type comparison and warning logs

--- src/services/filetype.rs
+++ src/services/filetype.rs
@@ -2,6 +2,7 @@ use actix_multipart::form::tempfile::TempFile;
 use infer;
 use mime::Mime;
 use std::{fs::File, io::Read, path::PathBuf};
+use tracing::warn;
 
 #[derive(Debug)]
 pub struct FileTypeInfo {
@@ -28,29 +29,50 @@ pub fn detect_file_type(file: &TempFile) -> FileTypeInfo {
 
     // try to infer by MIME type
     if let Some(kind) = infer::get(&buffer[..bytes_read]) {
-        return FileTypeInfo {
+        let file_type_info = FileTypeInfo {
             mime_type: kind.mime_type().parse().unwrap(),
             extension: kind.extension().to_string(),
         };
+
+        compare_mime_type(&file_type_info.mime_type, &browser_mime);
+
+        return file_type_info;
     }
 
     // check if it's plain text
     let is_text = std::str::from_utf8(&buffer[..bytes_read]).is_ok();
     if is_text {
-        return FileTypeInfo {
+        let file_type_info = FileTypeInfo {
             mime_type: plaintext_extension_to_mime(&original_file_extension),
             extension: original_file_extension,
         };
+
+        compare_mime_type(&file_type_info.mime_type, &browser_mime);
+
+        return file_type_info;
     }
 
-    // TODO: check the inferred MIME type against the MIME type given by the request
+    // TODO: should probably have a whitelist of MIME types that are allowed and just deny others
 
-    // TODO: final fallback: mark it as suspicious and return info from request
-    // if it's still unknown log it as suspicious and add the original extension
+    // if it's still unknown log it as suspicious and add the original MIME type and extension
+    warn!("Unable to infer MIME type. Given: {}", browser_mime);
 
     FileTypeInfo {
         mime_type: browser_mime,
         extension: original_file_extension,
+    }
+}
+
+fn compare_mime_type(inferred: &Mime, given: &Mime) -> bool {
+    if inferred == given {
+        true
+    } else {
+        warn!(
+            "Given MIME type doesn't match inferred MIME type. Inferred: {}, given: {}",
+            inferred, given
+        );
+
+        false
     }
 }
 
